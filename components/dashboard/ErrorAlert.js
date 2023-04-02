45a1fcf927889300927a2c756dbed20fb2d62184
Armaan Gupta <armaan@guppy.im>
2023-04-02 03:24:16 -0500

    feat: created error alert

---
+++ components/dashboard/ErrorAlert.js
@@ -0,0 +1,15 @@
+import { XCircleIcon } from "@heroicons/react/20/solid";
+export default function ErrorAlert({ message }) {
+  return (
+    <div className="border-l-4 border-red-400 bg-red-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <XCircleIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+        </div>
+        <div className="ml-3">
+          <p className="text-sm text-red-700">{message}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
