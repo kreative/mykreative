2da170ddf3a6e174d23c2a2a05d5dab29a61a0da
Armaan Gupta <armaan@guppy.im>
2023-04-03 09:02:46 -0500

    fix: icon color for error alert

--- components/dashboard/ErrorAlert.js
+++ components/dashboard/ErrorAlert.js
@@ -4,7 +4,7 @@ export default function ErrorAlert({ message }) {
     <div className="border-l-4 border-red-400 bg-red-50 p-4">
       <div className="flex">
         <div className="flex-shrink-0">
-          <XCircleIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+          <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
         </div>
         <div className="ml-3">
           <p className="text-sm text-red-700">{message}</p>
