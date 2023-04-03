1d66bc95a05ecee4cf1dfc2a8972436f9d6de07f
Armaan Gupta <armaan@guppy.im>
2023-04-03 09:03:16 -0500

    feat: created success alert component

---
+++ components/dashboard/SuccessAlert.js
@@ -0,0 +1,19 @@
+import { CheckCircleIcon } from "@heroicons/react/20/solid";
+
+export default function WarnAlert({ message }) {
+  return (
+    <div className="border-l-4 border-green-400 bg-green-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <CheckCircleIcon
+            className="h-5 w-5 text-green-400"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="ml-3">
+          <p className="text-sm text-green-700">{message}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
