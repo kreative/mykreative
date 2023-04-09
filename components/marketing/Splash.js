ac8c34a907b9a1200d1a362e209d008b9f736e2d
Armaan Gupta <armaan@guppy.im>
2023-04-09 08:08:35 -0500

    fix: removed duplicate shadow

--- components/marketing/Splash.js
+++ components/marketing/Splash.js
@@ -64,7 +64,7 @@ export default function Splash() {
           <div className="mt-10 flex items-center gap-x-6">
             <Link
               href="/account"
-              className="shadow-lg rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="shadow-lg rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Access MyKreative
             </Link>
