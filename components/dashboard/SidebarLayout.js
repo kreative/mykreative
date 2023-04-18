0f096a42bd3d7ca6ece56bf741ea0f1a3eed7df7
Armaan Gupta <armaan@guppy.im>
2023-04-18 12:07:15 -0500

    ref: inner content is lower on page

--- components/dashboard/SidebarLayout.js
+++ components/dashboard/SidebarLayout.js
@@ -253,7 +253,7 @@ export default function SidebarLayout({ title, subtitle, children }) {
           </div>
           <main className="flex-1">
             <div className="py-6">
-              <div className="mx-auto max-w-7xl pb-12 px-4 sm:px-6 lg:px-8">
+              <div className="mx-auto max-w-7xl pt-8 pb-12 px-4 sm:px-6 lg:px-8">
                 <h1 className="text-2xl font-semibold text-gray-900 pb-2">
                   {title}
                 </h1>
